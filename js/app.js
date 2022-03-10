7fa58f8255192f3955585f9228a38168fab99cd2
Matt K <matthedud@hotmail.com>
2022-03-10 17:11:37 +0100

    new maze

--- js/app.js
+++ js/app.js
@@ -21,7 +21,7 @@ backgroundMusic.loop
 const canvas = document.createElement("canvas")
 canvas.height = canvasHeight
 canvas.width = canvasWidth
-let game
+let game = null
 const ctx = canvas.getContext("2d")
 
 parentEl.appendChild(canvas)
@@ -48,7 +48,7 @@ closeControllerButton.addEventListener("click", hideController)
 form.addEventListener("submit", startGame)
 
 function showSettings() {
-	game.chronometer.stop()
+	game?.chronometer?.stop()
 	pauseGame = true
 	controllerButton.disabled = true
 	form.classList.add("visible")
@@ -88,6 +88,7 @@ function startGame(event) {
 	backgroundMusic.play();
 	controllerButton.disabled = false
 	const map = maze[event.target[3].value]
+	const players = game?.players ? game.players : []
 	switch (event.target[0].value) {
 		case "1":
 			game = new Shooter(map)
@@ -99,31 +100,34 @@ function startGame(event) {
 			game = new MegaShooter(map)
 			break
 	}
-	let randomCoord = game.randomPlacement()
-	const player1 = new Player(
-		event.target[1].value,
-		randomCoord.x,
-		randomCoord.y,
-		Math.random() * 2 * Math.PI,
-		randomColor(),
-		game.players.length,
-		'cat',
-	)
-	player1.controller = keyboards[0]
+	if (players.length <1) {
+		let randomCoord = game.randomPlacement()
+		const player1 = new Player(
+			event.target[1].value,
+			randomCoord.x,
+			randomCoord.y,
+			Math.random() * 2 * Math.PI,
+			randomColor(),
+			players.length,
+			'cat',
+			)
 
-	randomCoord = game.randomPlacement()
-	game.players.push(player1)
-	const player2 = new Player(
-		event.target[2].value,
-		randomCoord.x,
-		randomCoord.y,
-		Math.random() * 2 * Math.PI,
-		randomColor(),
-		game.players.length,
-	)
-	player2.controller = keyboards[1]
-	game.players.push(player2)
-	
+			player1.controller = keyboards[0]
+		
+			randomCoord = game.randomPlacement()
+			players.push(player1)
+			const player2 = new Player(
+				event.target[2].value,
+				randomCoord.x,
+				randomCoord.y,
+				Math.random() * 2 * Math.PI,
+				randomColor(),
+				players.length,
+			)
+			player2.controller = keyboards[1]
+			players.push(player2)
+	}
+	game.players = players
 	game.runGameLoop()
 	game.chronometer.start(clockEl)
 	numberOfRays = event.target[4].value > 2000 ? 2000 : event.target[4].value
